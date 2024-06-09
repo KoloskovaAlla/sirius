d9092698fd3bdbabf266e3a36b058d98f73cf1da
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2024-06-09 18:32:15 +0300

    WIP Header

---
+++ src/shared/icons/index.js
@@ -0,0 +1,9 @@
+//@ts-nocheck
+export { ReactComponent as IconSun } from './svg/sun.svg';
+export { ReactComponent as IconMoon } from './svg/moon.svg';
+export { ReactComponent as IconLogoHeader } from './svg/logoHeader.svg';
+export { ReactComponent as IconLogoFooter } from './svg/logoFooter.svg';
+export { ReactComponent as IconClose } from './svg/close.svg';
+export { ReactComponent as IconArrow } from './svg/arrowIcon.svg';
+export { ReactComponent as IconApple } from './svg/btn-apple.svg';
+export { ReactComponent as IconGoogle } from './svg/btn-google.svg';
